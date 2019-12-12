227e82830f2cdd6f51a025007dbee3ec18802652
Joel Yoder <github@joelyoder.com>
2019-12-12 01:48:10 -0500

    Added in conditional visibility logic to dashboard cards

--- project-campfire-dashboard.php
+++ project-campfire-dashboard.php
@@ -13,7 +13,7 @@ get_header();
 <script type="text/javascript">
     (function($) {
     $(window).load(function() {
-        $('.post-module').hover(function() {
+        $('.card-module').hover(function() {
         $(this).find('.description').stop().animate({
             height: "toggle",
             opacity: "toggle"
@@ -48,62 +48,55 @@ $posts = get_posts(array(
 
                 ?>
 
-                <?php
-                if( !empty($tags) ):
-                echo do_shortcode("[accessally_has_any_tag tag_id='$tags']");
-                endif;
-                ?>
+                <?php if( accessally_has_any_tag_id("$tags") || empty($tags) ): ?>
 
-                <div class="header-card">
+                    <div class="header-card">
 
-                    <div class="message">
-                        <h1><strong>Project</strong> Campfire <i class="fad fa-fire-alt" style="--fa-primary-color: #FFCA51; --fa-secondary-color: #fc7405; --fa-secondary-opacity: 1; font-size: 30px;" aria-hidden="true"></i></h1>
-                        <?php echo $message; ?>
+                        <div class="message">
+                            <h1><strong>Project</strong> Campfire <i class="fad fa-fire-alt" style="--fa-primary-color: #FFCA51; --fa-secondary-color: #fc7405; --fa-secondary-opacity: 1; font-size: 30px;" aria-hidden="true"></i></h1>
+                            <?php echo $message; ?>
 
-                        <!--button repeater-->
-                        <?php if( have_rows('buttons') ): ?>
+                            <!--button repeater-->
+                            <?php if( have_rows('buttons') ): ?>
 
-                            <p class="button-bar">
+                                <p class="button-bar">
 
-                                <?php while( have_rows('buttons') ): the_row();
+                                    <?php while( have_rows('buttons') ): the_row();
 
-                                //vars
-                                $text = get_sub_field('text');
-                                $url = get_sub_field('url');
-                                $color = get_sub_field('color');
-                                ?>
+                                    //vars
+                                    $text = get_sub_field('text');
+                                    $url = get_sub_field('url');
+                                    $color = get_sub_field('color');
+                                    ?>
 
-                                <a class="button <?php echo $color; ?>" href="<?php echo $url; ?>"><?php echo $text; ?></a>
+                                    <a class="button <?php echo $color; ?>" href="<?php echo $url; ?>"><?php echo $text; ?></a>
 
-                                <?php endwhile; ?>
+                                    <?php endwhile; ?>
 
-                            </p>
+                                </p>
 
-                        <?php endif; ?>
+                            <?php endif; ?>
 
-                        <!--unlocks counter-->
-                        <?php if( !empty($unlock_id) ): ?>
-                            <p><span style="display: inline-block;"><span class="count"><?php echo do_shortcode("[accessally_field_value operation_id='$operation_id']"); ?></span> <strong>UNLOCKS AVAILABLE</strong></span></p>
-                        <?php endif; ?>
+                            <!--unlocks counter-->
+                            <?php if( !empty($unlock_id) ): ?>
+                                <p><span style="display: inline-block;"><span class="count"><?php echo do_shortcode("[accessally_field_value operation_id='$operation_id']"); ?></span> <strong>UNLOCKS AVAILABLE</strong></span></p>
+                            <?php endif; ?>
+                        </div>
+
+                        <div class="video">
+                            <div class="embed-container">
+                                <?php echo $video ?>
+                            </div><!-- .embed-container -->
+                        </div>
+
                     </div>
 
-                    <div class="video">
-                        <div class="embed-container">
-                            <?php echo $video ?>
-                        </div><!-- .embed-container -->
-                    </div>
-
-                </div>
-
-                <?php
-                if( !empty($tags) ) :
-                echo do_shortcode("[/accessally_has_any_tag]");
-                endif;
-                ?>
+                <?php endif; ?>
 
             <?php endwhile; ?>
 
         <?php endif; ?>
+    </div><!-- .section -->
 
     <div class="section campfire">
 
@@ -113,7 +106,10 @@ $posts = get_posts(array(
                 <?php foreach( $posts as $post ): 
                     
                     setup_postdata( $post );
-                    $description = get_field( 'description' );
+                    $description = get_field('description');
+                    $freebie = get_field('freebie');
+                    $operation_id = get_field('operation_id');
+                    $tag = get_field('tag');
 
                     $domain_attrs = get_the_terms( $post->ID, 'domain' );
                     if (!empty($domain_attrs)){
@@ -123,7 +119,7 @@ $posts = get_posts(array(
                     }
                     ?>
 
-                    <div class="post-module">
+                <div class="card-module<?php if( !accessally_has_any_tag_id("104,2207,$tag") && !$freebie ): ?> disabled<?php endif; ?>" <?php if( accessally_has_any_tag_id("$tag") || $freebie ): ?>style="order:-1;"<?php endif; ?>>
 
                         <!-- Thumbnail-->
                         <div class="thumbnail">
@@ -155,8 +151,8 @@ $posts = get_posts(array(
                             endif; ?>
                         </div>
 
-                        <!-- Post Content-->
-                        <div class="post-content">
+                        <!-- Card Content-->
+                        <div class="card-content">
 
                             <div class="category <?php echo $domain; ?>">
                                 <?php echo $domain; ?>
@@ -182,7 +178,17 @@ $posts = get_posts(array(
                                     ?>
                                 </li>
                             </ul>
-                            <div class="post-meta"><a class="button" href="<?php the_permalink(); ?>">Access<i class="fas fa-fire-alt"></i></a></div>
+                            <div class="card-access">
+                                <?php if( accessally_has_any_tag_id("$tag") ): ?>
+                                    <a class="button" href="<?php the_permalink(); ?>">Access <i class="fas fa-fire-alt"></i></a>
+                                <?php elseif( $freebie ): ?>
+                                    <a class="button" href="<?php the_permalink(); ?>">FREE <i class="fas fa-fire-alt"></i></a>
+                                <?php elseif( accessally_has_any_tag_id("104,2207") ) : ?>
+                                    <?php echo do_shortcode("[accessally_field_value operation_id='$operation_id']"); ?>
+                                <?php else : ?>
+                                    <a class="button" href="">Coming Soon <i class="fas fa-lock"></i></a>
+                                <?php endif; ?>
+                            </div>
                         </div>
 
                     </div>
