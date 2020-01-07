a343b1960f997f1468cba2c3a4cdaaf472240b1d
Joel Yoder <github@joelyoder.com>
2020-01-06 22:53:43 -0500

    Allowed multi-select on filters

--- js/isotope.js
+++ js/isotope.js
@@ -13,33 +13,36 @@ jQuery(function($) {
         sortBy: [ 'owned', 'title' ]
     });
 
-    $('.button-group').on( 'click', 'button', function() {
-        var filterValue = $(this).attr('data-filter');
-        $grid.isotope({ filter: filterValue });
-    });
-
     // store filter for each group
-    var filters = {};
+    var filters = [];
 
-    $('.button-group').on( 'click', '.filter-button', function() {
-    var $this = $(this);
-    // get group key
-    var $buttonGroup = $this.parents('.button-group');
-    var filterGroup = $buttonGroup.attr('data-filter-group');
-    // set filter for group
-    filters[ filterGroup ] = $this.attr('data-filter');
-    // combine filters
-    var filterValue = concatValues( filters );
-    $grid.isotope({ filter: filterValue });
-    });
-
-    // flatten object by concatting values
-    function concatValues( obj ) {
-    var value = '';
-    for ( var prop in obj ) {
-        value += obj[ prop ];
+    // change is-checked class on buttons
+    $('.filters').on( 'click', 'button', function( event ) {
+    var $target = $( event.currentTarget );
+    $target.toggleClass('is-checked');
+    var isChecked = $target.hasClass('is-checked');
+    var filter = $target.attr('data-filter');
+    if ( isChecked ) {
+        addFilter( filter );
+    } else {
+        removeFilter( filter );
     }
-    return value;
+    // filter isotope
+    // group filters together, inclusive
+    $grid.isotope({ filter: filters.join(',') });
+    });
+    
+    function addFilter( filter ) {
+    if ( filters.indexOf( filter ) == -1 ) {
+        filters.push( filter );
+    }
+    }
+
+    function removeFilter( filter ) {
+    var index = filters.indexOf( filter);
+    if ( index != -1 ) {
+        filters.splice( index, 1 );
+    }
     }
 
     // quick search regex
