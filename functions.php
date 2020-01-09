6ec67096b1d2790a0edaaadfb2870d7d2c6f1f21
Joel Yoder <github@joelyoder.com>
2020-01-08 21:08:40 -0500

    Removed superfluous version number

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ function mte_enqueue_styles() {
  
     $parent_style = 'divi-style';
  
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css?v=2' );
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style ),
