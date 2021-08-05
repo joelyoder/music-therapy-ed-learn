ef60d7f20cb05820479b7a20d736768628d2a0e0
Joel Yoder <github@joelyoder.com>
2021-08-05 13:19:30 -0400

    Fixed sticky sidebar

--- single-intervention.php
+++ single-intervention.php
@@ -52,57 +52,59 @@ if (!empty($domain_attrs)){
 			<p><?php echo $description ?></p>
 		</div><!-- .intervention-header -->
 		<div class="intervention-sidebar">
-			<a href="/project-campfire" class="intervention-sidebar-nav"><i class="fas fa-fire-alt"></i>&nbsp;&nbsp;<strong>Project</strong> Campfire</a>
-			<h2>Domain</h2>
-			<?php
-			if (!empty($domain_attrs)) : ?>
-				<ul class="meta">
-					<?php 
-					foreach($domain_attrs as $attr){
-					echo '<li>' . $attr->name . '</li>';
-					}    
-					?>
-				</ul>
-			<?php endif; ?>
-			<h2>Methods</h2>
-			<?php
-			$method_attrs = get_the_terms( $post->ID, 'method' );
-			if (!empty($method_attrs)) : ?>
-				<ul class="meta">
-					<?php 
-					foreach($method_attrs as $attr){
-					echo '<li>' . $attr->name . '</li>';
-					}    
-					?>
-				</ul>
-			<?php endif; ?>
-			<h2>Populations</h2>
-			<?php
-			$population_attrs = get_the_terms( $post->ID, 'population' );
-			if (!empty($population_attrs)) : ?>
-				<ul class="meta">
-					<?php 
-					foreach($population_attrs as $attr){
-					echo '<li>' . $attr->name . '</li>';
-					}    
-					?>
-				</ul>
-			<?php endif; ?>
-			<h2>Equipment</h2>
-			<?php
-			$equipment_attrs = get_the_terms( $post->ID, 'equipment' );
-			if (!empty($equipment_attrs)) : ?>
-				<ul class="meta">
-					<?php 
-					foreach($equipment_attrs as $attr){
-					echo '<li>' . $attr->name . '</li>';
-					}    
-					?>
-				</ul>
-			<?php endif; ?>
-			<?php if(get_field('button_visibility')) : ?>
-				<a class="button navy" href="<?php the_field('button_url') ?>" target="_blank" rel="noopener"><?php the_field('button_text') ?></a>
-			<?php endif; ?>
+			<div class="sticky-container">
+				<a href="/project-campfire" class="intervention-sidebar-nav"><i class="fas fa-fire-alt"></i>&nbsp;&nbsp;<strong>Project</strong> Campfire</a>
+				<h2>Domain</h2>
+				<?php
+				if (!empty($domain_attrs)) : ?>
+					<ul class="meta">
+						<?php 
+						foreach($domain_attrs as $attr){
+						echo '<li>' . $attr->name . '</li>';
+						}    
+						?>
+					</ul>
+				<?php endif; ?>
+				<h2>Methods</h2>
+				<?php
+				$method_attrs = get_the_terms( $post->ID, 'method' );
+				if (!empty($method_attrs)) : ?>
+					<ul class="meta">
+						<?php 
+						foreach($method_attrs as $attr){
+						echo '<li>' . $attr->name . '</li>';
+						}    
+						?>
+					</ul>
+				<?php endif; ?>
+				<h2>Populations</h2>
+				<?php
+				$population_attrs = get_the_terms( $post->ID, 'population' );
+				if (!empty($population_attrs)) : ?>
+					<ul class="meta">
+						<?php 
+						foreach($population_attrs as $attr){
+						echo '<li>' . $attr->name . '</li>';
+						}    
+						?>
+					</ul>
+				<?php endif; ?>
+				<h2>Equipment</h2>
+				<?php
+				$equipment_attrs = get_the_terms( $post->ID, 'equipment' );
+				if (!empty($equipment_attrs)) : ?>
+					<ul class="meta">
+						<?php 
+						foreach($equipment_attrs as $attr){
+						echo '<li>' . $attr->name . '</li>';
+						}    
+						?>
+					</ul>
+				<?php endif; ?>
+				<?php if(get_field('button_visibility')) : ?>
+					<a class="button navy" href="<?php the_field('button_url') ?>" target="_blank" rel="noopener"><?php the_field('button_text') ?></a>
+				<?php endif; ?>
+			</div>
 		</div><!-- .intervention-sidebar -->
 		<div class="intervention-content">
 			<h2>Instructions</h2>
